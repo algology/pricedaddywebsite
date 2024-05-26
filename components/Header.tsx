c64ea619e1be388f2631e83036d03bd77b3d1fea
algology <alan.agon@unswalumni.com>
2024-05-26 15:50:55 +0930

    need a way to repro this

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,34 +2,36 @@
 import React from "react";
 import Image from "next/image";
 import logo from "../public/logo.svg";
-import { Button } from "./ui/button";
 import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/90 bg-background/40 backdrop-blur supports-\[backdrop-filter\]:bg-background/60">
-      <div className="container mx-auto flex h-14 items-center px-4 max-w-7xl">
+    <header className="sticky top-0 z-50 w-full border-b border-border/90 bg-background/40 backdrop-blur-md supports-[backdrop-filter]:bg-background/60">
+      <div className="container mx-auto flex h-24 items-center py-4 max-w-7xl">
         <div className="flex items-center">
           <Link href="/" className="flex items-center logo-animation">
-            <Button
-              variant="ghost"
-              size="icon"
-              className="hover:bg-transparent"
-            >
+            <button className="hover:bg-transparent">
               <Image
                 src={logo}
                 alt="Logo"
-                width={40}
-                height={40}
+                width={50}
+                height={50}
                 className="dark:rotate-0 transition-all antialiased"
               />
-            </Button>
-            <span className="hidden ml-2 font-bold sm:inline-block">
+            </button>
+            <span className="ml-2 font-bold lg:text-3xl text-2xl px-2">
               PriceDaddy!
             </span>
           </Link>
         </div>
         <div className="flex-grow"></div>
+        <div>
+          <Link href="https://chromewebstore.google.com/detail/price-daddy/aaafhonbegjjedhpomjjjpodjaakdbfc">
+            <button className="max-w-auto bg-black text-white text-[16px] font-medium border rounded-full sm:py-[20px] sm:px-[68px] py-[5px] px-[30px]">
+              Get it
+            </button>
+          </Link>
+        </div>
       </div>
     </header>
   );
